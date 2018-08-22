fe4a46ffbb4f93fd5cc0135129d18dc372f657ed
Mike Bertram <contact@mibexx.de>
2018-08-22 10:41:18 +0200

    * Test fixes

--- tests/Xervice/XerviceCli/Generator/IntegrationTest.php
+++ tests/Xervice/XerviceCli/Generator/IntegrationTest.php
@@ -4,14 +4,12 @@ namespace XerviceTest\XerviceCli\Generator;
 
 use Xervice\Core\Business\Model\Locator\Dynamic\Business\DynamicBusinessLocator;
 
+/**
+ * @method \Xervice\XerviceCli\Business\XerviceCliFacade getFacade()
+ */
 class IntegrationTest extends \Codeception\Test\Unit
 {
     use DynamicBusinessLocator;
-
-    /**
-     * @var \XerviceTest\XerviceTester
-     */
-    protected $tester;
 
     protected function _after(): void
     {
@@ -23,22 +21,38 @@ class IntegrationTest extends \Codeception\Test\Unit
      * @group Xervice
      * @group XerviceCli
      * @group Generator
+     * @group ProjectGenerator
+     * @group Integration
+     */
+    public function testProjectGenerator()
+    {
+        $this->getFacade()->createNewProject('UnitTest', 'TestNamespace');
+        $this->assertDirectoryExists(getcwd() . '/UnitTest');
+        $this->assertFileExists(getcwd() . '/UnitTest/.scrutinizer.yml');
+        $this->assertFileExists(getcwd() . '/UnitTest/.travis.yml');
+        $this->assertFileExists(getcwd() . '/UnitTest/README.md');
+        $this->assertFileExists(getcwd() . '/UnitTest/config/config_default.php');
+        $this->assertFileExists(getcwd() . '/UnitTest/src/TestNamespace/UnitTest/Persistence/UnitTestDataReader.php');
+        $this->assertFileExists(getcwd() . '/UnitTest/src/TestNamespace/UnitTest/Persistence/UnitTestDataWriter.php');
+        $this->assertFileExists(getcwd() . '/UnitTest/src/TestNamespace/UnitTest/Communication/UnitTestCommunicationFactory.php');
+        $this->assertFileExists(getcwd() . '/UnitTest/src/TestNamespace/UnitTest/Business/UnitTestBusinessFactory.php');
+        $this->assertFileExists(getcwd() . '/UnitTest/src/TestNamespace/UnitTest/Business/UnitTestFacade.php');
+        $this->assertFileExists(getcwd() . '/UnitTest/src/TestNamespace/UnitTest/UnitTestConfig.php');
+    }
+
+    /**
+     * @group Xervice
+     * @group XerviceCli
+     * @group Generator
      * @group ServiceGenerator
      * @group Integration
      */
     public function testServiceGenerator()
     {
-        $this->getFacade()->createNewProject('UnitTest', 'TestNamespace');
-        $this->assertTrue(is_dir(getcwd() . '/UnitTest'));
-        $this->assertTrue(is_file(getcwd() . '/UnitTest/.scrutinizer.yml'));
-        $this->assertTrue(is_file(getcwd() . '/UnitTest/.travis.yml'));
-        $this->assertTrue(is_file(getcwd() . '/UnitTest/README.md'));
-        $this->assertTrue(is_file(getcwd() . '/UnitTest/config/config_default.php'));
-
-        $this->rrmdir(getcwd() . '/UnitTest');
-
         $this->getFacade()->createNewService('Unit-Test', 'TestNamespace');
-        $this->assertTrue(is_dir(getcwd() . '/UnitTest'));
+        $this->assertDirectoryExists(getcwd() . '/UnitTest');
+        $this->assertFileExists(getcwd() . '/UnitTest/UnitTestConfig.php');
+        $this->assertFileExists(getcwd() . '/UnitTest/Communication/UnitTestCommunicationFactory.php');
     }
 
     /**
@@ -47,7 +61,7 @@ class IntegrationTest extends \Codeception\Test\Unit
     private function rrmdir($dir)
     {
         if (\is_dir($dir)) {
-            $objects = \scandir($dir);
+            $objects = \scandir($dir, SCANDIR_SORT_ASCENDING);
             foreach ($objects as $object) {
                 if ($object != "." && $object != "..") {
                     if (\is_dir($dir . "/" . $object)) {
